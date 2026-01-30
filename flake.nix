af916224640955a7c27bcb41d01d9af4cc2b924e
34j <34j.github@proton.me>
2026-01-31 00:31:28 +0900

    chore: inherit system

--- flake.nix
+++ flake.nix
@@ -16,7 +16,10 @@
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = nixpkgs.legacyPackages."${system}";
+        pkgs = (import nixpkgs) {
+          inherit system;
+          config.allowUnfreePredicate = pkg: true;
+        };
       in
       {
         packages = {
